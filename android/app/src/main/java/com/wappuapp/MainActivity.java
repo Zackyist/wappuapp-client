f56bc51743894aa9b78993aeb9b95c0dbaae956e
Pasi Lampinen <pasi.lampinen@futurice.com>
2017-01-31 22:31:17 +0200

    Update React Native to 0.39

--- android/app/src/main/java/com/wappuapp/MainActivity.java
+++ android/app/src/main/java/com/wappuapp/MainActivity.java
@@ -1,18 +1,15 @@
 package com.wappuapp;
 
 import com.facebook.react.ReactActivity;
-import com.microsoft.codepush.react.CodePush;
-import com.imagepicker.ImagePickerPackage;
-import com.AirMaps.AirPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.learnium.RNDeviceInfo.*;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.slowpath.hockeyapp.RNHockeyAppModule;
 import com.slowpath.hockeyapp.RNHockeyAppPackage;
-
-import java.util.Arrays;
-import java.util.List;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.image.zoom.ReactImageZoom;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.AirMaps.AirPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainActivity extends ReactActivity {
 
@@ -23,31 +20,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "wappuapp";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-          new RNHockeyAppPackage(this),
-          new MainReactPackage(),
-            new CodePush("8NxIp9cI4yfPOOUkk6AR1BOr5gjgNy6by4LyW", this, BuildConfig.DEBUG),
-          new ImagePickerPackage(),
-          new RNDeviceInfo(),
-          new AirPackage(),
-          new VectorIconsPackage()
-        );
     }
 }
