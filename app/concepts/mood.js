0044a51812a439916daff5f036dda6664d660df9
Pasi Lampinen <pasi.lampinen@futurice.com>
2017-03-13 17:16:06 +0200

    Mood concept for getting wappufiilis data
    - currently no api-calls, just placeholder data

---
+++ app/concepts/mood.js
@@ -0,0 +1,60 @@
+import { createSelector } from 'reselect';
+import { fromJS } from 'immutable';
+import moment from 'moment';
+import { times } from 'lodash';
+
+
+// # Action creators
+export const SET_MOOD_DATA = 'mood/SET_MOOD_DATA';
+export const setMoodData = (mood) => ({ type: SET_MOOD_DATA, payload: mood });
+
+
+// # Selectors
+export const getMoodData = state => state.mood.get('data');
+export const getLimitLine = state => state.mood.get('limitLine');
+
+
+const processMoodData = data => data;
+
+export const getMoodDataForChart = createSelector(
+  getMoodData,
+  (data) => processMoodData(data)
+);
+
+export const getLimitLineData = createSelector(
+  getLimitLine, getMoodDataForChart,
+  (limit, data) => data.map(item => ({
+    date: item.get('date'),
+    value: limit
+  }))
+);
+
+export const getKpiValues = state => fromJS({
+  own: 33.6,
+  team: 44.1,
+  total: 58.5,
+});
+
+
+// # Reducer
+const placeHolderMoodData = times(27).map((item, index) => ({
+  date: moment('2017-05-02').subtract(index, 'd').format('YYYY-MM-DD'),
+  avg: index > 8 ? (70 - index * 2) + Math.floor(Math.random() * 5) : null
+})).reverse();
+
+
+const initialState = fromJS({
+  data: placeHolderMoodData,
+  limitLine: 50,
+});
+
+export default function mood(state = initialState, action) {
+  switch (action.type) {
+    case SET_MOOD_DATA: {
+      return state.set('data', action.payload);
+    }
+    default: {
+      return state;
+    }
+  }
+}
