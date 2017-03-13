ef553d8b872c3026ef7b120e770b7b02807e3b59
Emmi Siitonen <emmi.siitonen@futurice.com>
2017-03-13 16:47:12 +0200

    Selected city is saved to app on user creation.

--- app/actions/team.js
+++ app/actions/team.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import api from '../services/api';
-import {createRequestActionTypes} from '.'
+import {createRequestActionTypes} from '.';
+import { getCityId } from '../concepts/city';
 
 const {
   GET_TEAMS_REQUEST,
@@ -10,13 +11,20 @@ const {
 } = createRequestActionTypes('GET_TEAMS');
 const SHOW_TEAM_SELECTOR = 'SHOW_TEAM_SELECTOR';
 
-const fetchTeams = () => {
-  return dispatch => {
-    dispatch({ type: GET_TEAMS_REQUEST });
-    api.fetchModels('teams')
-      .then(teams => dispatch({ type: GET_TEAMS_SUCCESS, payload: teams }))
-      .catch(e => dispatch({ type: GET_TEAMS_FAILURE, error: true, payload: e }));
-  };
+const fetchTeams = () => (dispatch, getState) => {
+  let cityId = getCityId(getState());
+  // if (!cityId) {
+  //   return;
+  // }
+
+  if (cityId === 1) {
+    cityId = null;
+  }
+
+  dispatch({ type: GET_TEAMS_REQUEST });
+  return api.fetchModels('teams', cityId)
+    .then(teams => dispatch({ type: GET_TEAMS_SUCCESS, payload: teams }))
+    .catch(e => dispatch({ type: GET_TEAMS_FAILURE, error: true, payload: e }));
 };
 
 const showChooseTeam = () => {
