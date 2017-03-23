80881a40009b15e0f13357b5669f7ae943634adc
Emmi Siitonen <emmi.siitonen@futurice.com>
2017-03-23 17:22:29 +0200

    User can check-in to events through individual event pages.

--- app/constants/CheckInStates.js
+++ app/constants/CheckInStates.js
@@ -1,4 +1,5 @@
 // # Possible states for check-in butotn
+export const INACTIVE = 'INACTIVE';
 export const UNAVAILABLE = 'UNAVAILABLE';
 export const AVAILABLE = 'AVAILABLE';
 export const CHECKED = 'CHECKED';
