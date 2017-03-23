0bd7b10fb73b9803d23172fc804251eab2c7edf8
Pasi Lampinen <pasi.lampinen@futurice.com>
2017-03-23 10:57:36 +0200

    Add city icon to city selection in header
    Icons still WIP

--- app/components/header/CityToggle.ios.js
+++ app/components/header/CityToggle.ios.js
@@ -2,26 +2,45 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
+  Image,
+  View,
   TouchableHighlight
 } from 'react-native'
 
 import { connect } from 'react-redux';
 
-import { openCitySelection, toggleCityPanel, getCityId } from '../../concepts/city';
+import { openCitySelection, toggleCityPanel, getCityId, getCurrentCityName } from '../../concepts/city';
 
 import theme from '../../style/theme';
-import MdIcon from 'react-native-vector-icons/MaterialIcons';
+// import MdIcon from 'react-native-vector-icons/MaterialIcons';
+
+const cityIcons = {
+  'helsinki': require('../../../assets/cities/icon-ota-amfi3.png'),
+  'tampere': require('../../../assets/cities/icon-tampere.png')
+};
 
 const CitySelector = ({
   currentCity,
-  toggleCityPanel
+  toggleCityPanel,
+  currentCityName
 }) => (
   <TouchableHighlight
     underlayColor={'transparent'}
     onPress={() => toggleCityPanel() }>
-    <Text style={styles.filterText}>
-      <MdIcon name='location-city' style={styles.filterIcon} /> {/*currentCity*/}
+    <View>
+      <Image
+        source={(currentCityName || '').toLowerCase() === 'tampere'
+          ? cityIcons.tampere
+          : cityIcons.helsinki
+        }
+        style={styles.cityIcon}
+      />
+    </View>
+    {/*
+     <Text style={styles.filterText}>
+      <MdIcon name='location-city' style={styles.filterIcon} /> {/*currentCity
     </Text>
+    */ }
   </TouchableHighlight>
 );
 
@@ -29,11 +48,18 @@ var styles = StyleSheet.create({
   filterText: {
     color: theme.white,
     fontSize: 10,
-    paddingTop: 10,
+    paddingTop: 15,
     paddingLeft: 18,
   },
   filterIcon: {
     fontSize: 24,
+  },
+  cityIcon: {
+    tintColor: theme.white,
+    top: 0,
+    left: 5,
+    width: 40,
+    height: 40,
   }
 });
 
@@ -43,6 +69,7 @@ const mapDispatchToProps = { openCitySelection, toggleCityPanel };
 const select = state => {
   return {
     currentCity: getCityId(state),
+    currentCityName: getCurrentCityName(state),
     currentTab: state.navigation.get('currentTab')
   }
 };
