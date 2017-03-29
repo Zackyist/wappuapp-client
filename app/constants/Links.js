4bf00ea4275ba542525927a682fc885a42738ec7
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-03-29 09:27:03 +0300

    Open most of links in browser instead of webview

--- app/constants/Links.js
+++ app/constants/Links.js
@@ -3,17 +3,17 @@ const ROOT_URL = 'https://wappu.futurice.com';
 const links = [
   {title: 'Fuksi Survival Kit', showCity: 'tampere', link: `https://ttyy.fi/me-ollaan-teekkareita/teekkarikulttuuri/wappu/fuksi-survival-kit/`, icon: 'local-hospital', showInWebview: true},
   {title: 'Feedback', mailto: 'wappu@futurice.com', icon: 'send'},
-  {title: 'Source Code', link: `https://github.com/futurice/wappuapp-client`, icon: 'code', showInWebview: true},
-  {title: 'from Tammerforce', showCity: 'tampere', link: `https://tammerforce.com`, icon: 'favorite-border', showInWebview: true},
+  {title: 'Source Code', link: `https://github.com/futurice/wappuapp-client`, icon: 'code', showInWebview: false},
+  {title: 'from Tammerforce', showCity: 'tampere', link: `https://tammerforce.com`, icon: 'favorite-border', showInWebview: false},
   {title: 'Wanna work at Futurice?',
     link: 'http://futurice.com/careers?utm_source=wappuapp&utm_medium=app&utm_campaign=wappu2017',
     icon: 'star', separatorAfter: true}
 ];
 
 const terms = [
-  {title: 'Terms of Service', link: `${ROOT_URL}/terms`, icon: 'info-outline'},
-  {title: 'Privacy', link: `${ROOT_URL}/privacy`, icon: 'lock-outline'},
-  {title: 'Licenses', link: `${ROOT_URL}/licenses`, icon: 'help-outline', last: true},
+  {title: 'Terms of Service', link: `${ROOT_URL}/terms`, icon: 'info-outline', showInWebview: false},
+  {title: 'Privacy', link: `${ROOT_URL}/privacy`, icon: 'lock-outline', showInWebview: false},
+  {title: 'Licenses', link: `${ROOT_URL}/licenses`, icon: 'help-outline', showInWebview: false, last: true},
 ];
 
 
