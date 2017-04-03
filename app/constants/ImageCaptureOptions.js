af1d2407a502b4f7ec7771d3b734f8af76cf470d
Pasi Lampinen <plam@fl027.local>
2017-04-03 15:14:52 +0300

    Reduce image quality
    In iOS 95% -> 100% quality 3x image size

--- app/constants/ImageCaptureOptions.js
+++ app/constants/ImageCaptureOptions.js
@@ -12,7 +12,7 @@ export default {
   maxHeight: 1000,
   aspectX: 2, // android only - aspectX:aspectY, the cropping image's ratio of width to height
   aspectY: 1, // android only - aspectX:aspectY, the cropping image's ratio of width to height
-  quality: 1, // 0 to 1, photos only
+  quality: 0.95, // 0 to 1, photos only
   angle: 0,
   allowsEditing: false, // Built in functionality to resize/reposition the image after selection
   noData: false, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
