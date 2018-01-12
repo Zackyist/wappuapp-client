6c21809e60484144d6e678b5d7f793ff2a26d6da
Santtu Laine <santtu.laine@iki.fi>
2018-01-12 13:04:45 +0200

    Changed react-native-star-rating dependacy to react-native-stars dependacy. Added star pictures to the assets folder. Navigation functional but not complete.

--- app/components/calendar/EventFeedbackForm.js
+++ app/components/calendar/EventFeedbackForm.js
@@ -1,14 +1,16 @@
 'use strict';
 
 // TODO: Styling of the TextInput
-// TODO: Waiting backend to send the feedback to
+// TODO: Waiting for backend to send the feedback to
 // TODO: Fix colour sources to theme
 // TODO: Repair navigation
+// TODO: Add event and userid to props
 
 import React, { Component } from 'react';
-import { Text, View, Navigator, TextInput } from 'react-native';
-import StarRating from 'react-native-star-rating';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
+import Star from 'react-native-stars';
 import Button from '../common/Button';
+import Toolbar from './EventDetailToolbar';
 import { connect } from 'react-redux';
 // import autobind from 'autobind-decorator';
 
@@ -30,28 +32,26 @@ class EventFeedback extends Component {
     });
   }
 
-  // @autobind
-  // renderScene(route, navigator) {
-  //   if (route.component) {
-  //     const RouteComponent = route.component;
-  //     return <RouteComponent route={route} {...this.props} />
-  //   }
-  // }
+  placeholder() {
+    return;
+  }
 
   render() {
     return (
-      <View>
+      <View style={{ backgroundColor: theme.lightgrey }} >
+        <Toolbar style={styles.toolbarStyle} title={'Event Feedback'} navigator={this.props.navigator} />
         <View style={ styles.containerStyle } >
           <Text style={ styles.textStyle } >
             Rate the event
           </Text>
-          <StarRating
-            maxStars={5}
-            rating={this.state.stars}
-            selectedStar={(rating) => this.giveStarRating(rating)}
-            starColor={'#00d7d2'}
-            emptyStarColor={'white'}
+          <Star
+            update={(val)=>{this.setState({stars: val})}}
+            spacing={4}
             starSize={40}
+            count={5}
+            fullStar={require('../../../assets/starFilled.png')}
+            emptyStar={require('../../../assets/starEmpty.png')}
+            halfStar={require('../../../assets/starHalf.png')}
           />
         </View>
         <View style={ styles.containerStyle } >
@@ -64,8 +64,8 @@ class EventFeedback extends Component {
             onChangeText={(text) => this.setState({text})}
           />
         </View>
-        <View style={styles.buttonWrapper}>
-            <Button onPress={() => this.giveFeedback.bind(this)}>Give feedback</Button>
+        <View style={styles.navigationButton}>
+            <Button onPress={() => this.placeholder()}>Send</Button>
         </View>
       </View>
     );
@@ -78,18 +78,22 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     margin: 16,
     justifyContent: 'center',
-
+    paddingBottom: 20
   },
   textStyle: {
     fontSize: 16,
-    fontColor: '#ec449c'
+    color: '#ec449c',
+    margin: 10
   },
-  buttonWrapper: {
-    margin: 15,
-    marginTop: 0,
-    marginBottom: 15,
-    borderRadius: 3,
-    backgroundColor: theme.light
+  navigationButton: {
+    height: 50,
+    backgroundColor: '#E9E9E9',
+    borderColor: '#C7C7C7',
+    borderRadius: 2,
+  },
+  toolbarStyle: {
+    opacity: 1,
+    marginBottom: 16
   }
 });
 
