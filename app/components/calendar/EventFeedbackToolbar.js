c001a55661bf7c53907315d892304f5525b4ce2b
Santtu Laine <santtu.laine@iki.fi>
2018-01-12 13:58:10 +0200

    Added new toolbar for feedback view's use. Tweaked styling of the feedback view.

---
+++ app/components/calendar/EventFeedbackToolbar.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import React, { Component, PropTypes } from 'react';
+import { ToolbarAndroid, StyleSheet } from 'react-native';
+import autobind from 'autobind-decorator';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import theme from '../../style/theme';
+
+// const toolbarActions = [
+//   {title: 'Share', id:'share'}
+// ];
+
+const styles = StyleSheet.create({
+  toolbar: {
+    backgroundColor: theme.secondary,
+    position:'absolute',
+    left:0,
+    top:0,
+    right:0,
+    elevation:2,
+    height: 56,
+    marginBottom: 15
+  }
+});
+
+class EventDetailToolbar extends Component {
+  propTypes: {
+    title: PropTypes.string.isRequired,
+    navigator: PropTypes.object.isRequired
+  }
+
+  @autobind
+  goBack() {
+    this.props.navigator.pop();
+  }
+
+  onActionSelected(position) {
+    //TODO switch toolbarActions[position]
+  }
+
+  render() {
+    const { backgroundColor } = this.props;
+
+    return (
+      <Icon.ToolbarAndroid
+      // actions={toolbarActions} TODO: SHARE
+      // onActionSelected={this._onActionSelected}
+      onIconClicked={this.goBack}
+      navIconName={'arrow-back'}
+      titleColor={theme.light}
+      iconColor={theme.light}
+      style={[styles.toolbar, backgroundColor ? { backgroundColor } : {} ]}
+      title={this.props.title}
+      />
+    );
+  }
+}
+
+module.exports = EventDetailToolbar;
